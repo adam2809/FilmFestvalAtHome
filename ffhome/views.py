932d070136133842cdfe5af69a92052244458b74
adam2809 <adam.kule@gmail.com>
2020-01-26 06:20:03 +0000

    Get films by fest by year works

--- ffhome/views.py
+++ ffhome/views.py
@@ -6,10 +6,9 @@ def get_festivals(request):
 
 
 def get_films_in_festival_by_year(request,name,year):
-    print(name)
     fest = Festival.objects.get(name=name)
-    films = Film.objects.get(year=year,festival_id=fest.id)
-    return JsonResponse({'films':[f.name for f in films]})
+    films = Film.objects.filter(festival_id=fest.id).filter(year=year)
+    return JsonResponse({'films':[{'name':f.name,'year':f.year,'award':f.award} for f in films]})
 
 
 
