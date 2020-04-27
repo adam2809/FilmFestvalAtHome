2d58b3a22ae7e8a67bf5b14ebb8be7e459db4e17
adam2809 <adam.kule@gmail.com>
2020-04-27 17:18:41 +0100

    finished refactoring scraper system

--- ffhome/management/commands/updatedb.py
+++ ffhome/management/commands/updatedb.py
@@ -1,48 +1,31 @@
-from django.core.management.base_scraper import BaseCommand
+from ffhome.management.commands.scrapers.awardsscraper import NetogramScraper
+from django.core.management.base import BaseCommand
 from ffhome.models import Film,Festival
-from ffhome.awardsscraper import get_awards_for_all_festivals
 
 class Command(BaseCommand):
+    film_id = 1
+
     def handle(self,*args, **options):
-        self.updateFestivals()
-        self.updateFilms()
-
-
-    def updateFestivals(self):
         Festival.objects.all().delete()
-
-        for i,fest_name in enumerate(
-                         ['Academy Awards - OSCARS',
-                          'Australian Film Institute Awards',
-                          'BAFTA Film Awards',
-                          'CESAR Film Awards',
-                          'Berlin International Film Festival',
-                          'Golden Globe Awards',
-                          'Venice International Film Festival',
-                          'Cannes International Film Festival',
-                          'Sundance Film Festival',
-                         ]):
-
-
-            try:
-                Festival(id=i+1,name=fest_name).save()
-            except Exception as e:
-                print(f'Could not save festival {fest_name}\nThe exception was:\n')
-                print(e)
-
-    def updateFilms(self):
         Film.objects.all().delete()
 
-        awards = get_awards_for_all_festivals()
+        for scraper in [NetogramScraper()]:
+            self.update(scraper)
 
-        saved_films_count = 0
+    def update(self,scraper):
+        festFilmDict = scraper.scrape()
 
-        for i,fest in enumerate(awards):
-            for film in awards[fest]:
+        for i,fest in enumerate(festFilmDict):
+            Festival(id=i+1,name=fest).save()
+
+            for film in festFilmDict[fest]:
                 try:
-                    f = Film(id=saved_films_count+1,name=film.name,year=film.year,award=film.award,festival_id=i+1)
-                    f.save()
-                    saved_films_count+=1
+                    f = Film(id=self.film_id,
+                             name=film.name,
+                             year=film.year,
+                             award=film.award,
+                             festival_id=i+1).save()
+                    self.film_id+=1
                 except Exception as e:
                     print(f'This film could not have been saved:\n{film}\nThe exception was:')
                     print(e)
