1095c9b7bec732dcb010d109280ed14d73604b90
adam2809 <adam.kule@gmail.com>
2020-05-16 16:07:56 +0200

    Removed testing endpoint

--- ffhome/views.py
+++ ffhome/views.py
@@ -10,9 +10,3 @@ def get_festivals(request):
 def get_films_in_festival(request,fest_id):
     films = Film.objects.filter(festival_id=fest_id)
     return JsonResponse({'films':[{'name':f.name,'year':f.year,'award':f.award} for f in films]})
-
-
-def testing(request):
-    with open('ffhome/testJustwatchResponse.json') as f:
-        j = json.loads(f.read())
-    return JsonResponse(j)
